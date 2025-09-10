4fb3ac6ea6f0fc2a6e1dafac9d2137bba401f4a5
Lello345 <118297410+Lello345@users.noreply.github.com>
2025-09-10 18:39:14 +0200

    Aggiunta services e controllers con implentazioni base
    AccountController, EsperienzaController, AccountService, PropostaService

--- app/src/main/java/it/unicam/cs/ids2425/filieraagricola/controller/AccountController.java
+++ app/src/main/java/it/unicam/cs/ids2425/filieraagricola/controller/AccountController.java
@@ -1,4 +1,60 @@
 package it.unicam.cs.ids2425.filieraagricola.controller;
 
+import it.unicam.cs.ids2425.filieraagricola.model.*;
+import it.unicam.cs.ids2425.filieraagricola.service.AccountService;
+
 public class AccountController {
+    AccountService accService = new AccountService();
+    ContenutoService conService = new ContenutoService();
+
+    public void creaUtente(String email, String password, String nome, String cognome){
+        Utente u = new Utente(email, password, nome, cognome);
+        accService.aggiungiUtente(u);
+    }
+    public void modificaUtente(String precEmail, String email, String password, String nome, String cognome){
+        Utente u = new Utente(email, password, nome, cognome);
+        //TODO finire
+        accService.modificaUtente();
+    }
+
+    public void rimuoviUtente(String email){
+        //TODO modifica
+        accService.rimuoviUtente(null);
+    }
+
+    public void creaVenditore(String email, String password, String PIVA, String ragioneFiscale, String descrizione){
+        Venditore v = new Venditore(email, password, PIVA, ragioneFiscale, descrizione);
+        accService.aggiungiVenditore(v);
+    }
+
+    public void modificaVenditore(String precEmail, String email, String password, String PIVA, String ragioneFiscale, String descrizione){
+        Venditore v = new Venditore(email, password, PIVA, ragioneFiscale, descrizione);
+        //TODO finire
+        accService.modificaVenditore();
+    }
+
+    public void rimuoviVenditore(String PIVA){
+        //TODO modifica
+        accService.rimuoviVenditore(null);
+    }
+
+    public Utente getUtente(String email){
+        return accService.getUtenteByEmail(email);
+    }
+
+    public Venditore getVenditore(String PIVA){
+        return accService.getVenditoreByPIVA(PIVA);
+    }
+
+    public Carrello getCarrelloUtente(String email){
+        return accService.getUtenteByEmail(email).getCarrello();
+    }
+
+    public void svuotaCarrello(String email){
+        accService.getUtenteByEmail(email).getCarrello().svuota();
+    }
+
+    public void aggiungiContenutoCarrello(String email, Contenuto c, int quant){
+        accService.getUtenteByEmail(email).getCarrello().aggiungiContenuto(new RigaCarrello(c, quant));
+    }
 }
--- app/src/main/java/it/unicam/cs/ids2425/filieraagricola/service/AccountService.java
+++ app/src/main/java/it/unicam/cs/ids2425/filieraagricola/service/AccountService.java
@@ -1,4 +1,63 @@
 package it.unicam.cs.ids2425.filieraagricola.service;
 
+import it.unicam.cs.ids2425.filieraagricola.model.Account;
+import it.unicam.cs.ids2425.filieraagricola.model.Ruolo;
+import it.unicam.cs.ids2425.filieraagricola.model.Utente;
+import it.unicam.cs.ids2425.filieraagricola.model.Venditore;
+
+import java.util.List;
+
 public class AccountService {
+    private List<Utente> utenti;
+    private List<Venditore> venditori;
+
+    public void aggiungiUtente(Utente u){
+        utenti.add(u);
+    }
+
+    public void rimuoviUtente(Utente u){
+        utenti.remove(u);
+    }
+
+    public void modificaUtente(){
+        //TODO
+    }
+
+    public void aggiungiVenditore(Venditore v){
+        venditori.add(v);
+    }
+
+    public void rimuoviVenditore(Venditore v){
+        venditori.remove(v);
+    }
+
+    public void modificaVenditore(){
+        //TODO
+    }
+
+    public void assegnaRuolo(String email, Ruolo r){
+        //TODO
+    }
+
+    public void assegnaRuoli(String email, List<Ruolo> r){
+        //TODO
+    }
+
+    public Utente getUtenteByEmail(String email){
+        //TODO
+        return null;
+    }
+
+    public Venditore getVenditoreByPIVA(String PIVA){
+        //TODO
+        return null;
+    }
+
+    public List<Utente> getUtenti() {
+        return utenti;
+    }
+
+    public List<Venditore> getVenditori() {
+        return venditori;
+    }
 }
